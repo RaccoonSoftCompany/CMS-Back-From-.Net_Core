51dac764be32f31feb0a3df28bda866292b36400
lin123 <1121943595@qq.com>
2021-07-27 16:30:26 +0800

    搭建CRUD初始框架

--- Article.CMS.Api/Controllers/UsersController.cs
+++ Article.CMS.Api/Controllers/UsersController.cs
@@ -36,5 +36,39 @@ namespace Article.CMS.Api.Controllers
                 Msg = "获取用户表成功！"
             });
         }
+
+        [HttpPost]
+        public dynamic Post()
+        {
+
+            return new
+            {
+                Code = 1000,
+                Data = "",
+                Msg = "添加成功"
+            };
+        }
+
+        [HttpPut]
+        public dynamic Put()
+        {
+            return new
+            {
+                Code = 1000,
+                Data = "",
+                Msg = "更新成功"
+            };
+        }
+
+        [HttpDelete]
+        public dynamic Delete()
+        {
+            return new 
+            {
+                Code=1000,
+                Data="",
+                Msg="删除成功"
+            };
+        }
     }
 }
