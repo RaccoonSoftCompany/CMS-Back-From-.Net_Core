ff57038a53f4b638c6d1cd45b97706a60a156b57
lin123 <1121943595@qq.com>
2021-07-27 11:04:21 +0800

    初步完成接口类，尚未实现接口

--- Article.CMS.Api/Repository/EfRepository.cs
+++ Article.CMS.Api/Repository/EfRepository.cs
@@ -1,19 +1,15 @@
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace Article.CMS.Api.Repository
 {
     public class EfRepository<T> : IRepository<T>
     {
-        
-
-
         public IQueryable<T> Table => throw new System.NotImplementedException();
 
         public void Delete(object id)
         {
-            var deDate =
+            throw new System.NotImplementedException();
         }
 
         public void DeleteBulk(object ids)
