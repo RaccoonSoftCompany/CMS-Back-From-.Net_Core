a7f33ae695d32cd7ece986c3f8944378911a060d
SFY <1473209499@qq.com>
2021-07-31 10:07:36 +0800

    完成用户信息编辑以及创建

--- Article.CMS.Api/Controllers/UserInfosController.cs
+++ Article.CMS.Api/Controllers/UserInfosController.cs
@@ -18,7 +18,7 @@ namespace Article.CMS.Api.Controllers
         /// 创建用户信息存储仓库
         /// </summary>
         /// <typeparam name="UserInfos">用户信息实体</typeparam>
-        /// <returns></returns>
+        /// <returns>状态码</returns>
         private IRepository<UserInfos> _UserInfosRepository;
 
         public UserInfosController(IConfiguration configuration, IRepository<UserInfos> UserInfosRepository)
@@ -27,21 +27,72 @@ namespace Article.CMS.Api.Controllers
         }
 
         /// <summary>
-        /// 获取用户信息表
+        /// 创建新的用户信息
         /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public dynamic Get()
+        /// <param name="id">用户id</param>
+        /// <param name="newUserInfos">传入前端数据实体</param>
+        /// <returns>状态码</returns>
+        [Route("newUserInfo/{id}")]
+        [HttpPost]
+        public dynamic newUserInfo(int id, UserInfoParams newUserInfos)
         {
-
             var userInfos = _UserInfosRepository.Table.ToList();
-            return JsonHelper.Serialize(new
+            var userInfo = userInfos.Where(x => x.UserId == id).SingleOrDefault();
+            if (userInfo != null)
             {
-                Code = 1000,
-                Data = userInfos,
-                Msg = "获取用户信息表成功！"
-            });
+                return DataStatus.DataError(1221, "该用户不是新用户！");
+            }
+
+            var newNickName = newUserInfos.NickName == "" ? "newU-" + id : newUserInfos.NickName.Trim();
+            var newSex = newUserInfos.Sex == "" ? null : newUserInfos.Sex.Trim();
+            if (newSex != null && !(newSex == "男" | newSex == "女"))
+            {
+                    return DataStatus.DataError(1222, "用户性别不正确请检查！");
+            }
+
+            var newUserInfo = new UserInfos
+            {
+                UserId = id,
+                NickName = newNickName,
+                Sex = newSex
+            };
+
+            _UserInfosRepository.Insert(newUserInfo);
+            return DataStatus.DataSuccess(1000, newUserInfo, "用户信息创建成功！");
         }
 
+        /// <summary>
+        /// 修改用户信息
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="PasswordInfo">传入前端数据实体</param>
+        /// <returns>状态码</returns>
+        [HttpPut]
+        [Route("ChangeUserInfo/{id}")]
+        public dynamic ChangeUserInfo(int id, UserInfoParams UpUserInfo)
+        {
+            var userInfos = _UserInfosRepository.Table.ToList();
+            var userInfo = userInfos.Where(x => x.UserId == id).SingleOrDefault();
+            if (userInfo == null)
+            {
+                return DataStatus.DataError(1221, "该用户不存在！");
+            }
+            var dbuserInfoNickName=userInfo.NickName;
+            var dbsex=userInfo.Sex;
+
+            var UpNickName = UpUserInfo.NickName == null ? dbuserInfoNickName : UpUserInfo.NickName.Trim();
+            var UpSex = UpUserInfo.Sex == null ? dbsex : UpUserInfo.Sex.Trim();
+            if (UpSex != null && !(UpSex == "男" | UpSex == "女"))
+            {
+                    return DataStatus.DataError(1222, "用户性别不正确请检查！");
+            }
+
+            userInfo.NickName=UpNickName;
+            userInfo.Sex=UpSex;
+
+            _UserInfosRepository.Update(userInfo);
+            return DataStatus.DataSuccess(1000, userInfo, "用户信息修改成功！");
+
+        }
     }
 }
