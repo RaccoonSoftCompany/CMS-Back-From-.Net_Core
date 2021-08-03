3229d0c0b22999b73a81a0d53e7ee430c26f7577
SFY <1473209499@qq.com>
2021-08-03 09:37:03 +0800

    完成后台修改问题接口，测试可用

--- Article.CMS.Api/Controllers/BackMatterController.cs
+++ Article.CMS.Api/Controllers/BackMatterController.cs
@@ -6,6 +6,7 @@ using Article.CMS.Api.Entity;
 using Article.CMS.Api.Utils;
 using System.Linq;
 using Article.CMS.Api.Params;
+using Article.CMS.Api.Database;
 
 namespace Article.CMS.Api.Controllers
 {
@@ -19,6 +20,11 @@ namespace Article.CMS.Api.Controllers
         /// <typeparam name="Matters">问题实体</typeparam>
         /// <returns></returns>
         private IRepository<Matters> _mattersRepository;
+        /// <summary>
+        /// 所有数据库
+        /// </summary>
+        /// <returns></returns>
+        ArticleCmsdb _Context = new ArticleCmsdb();
 
         public BackMatterController(IConfiguration configuration, IRepository<Matters> mattersRepository)
         {
@@ -43,7 +49,7 @@ namespace Article.CMS.Api.Controllers
             var dbmattet = matters.Where(x => x.MName.Equals(mName)).SingleOrDefault();
             if (dbmattet != null)
             {
-                return DataStatus.DataError(1331, "请勿添加相同问题！");
+                return DataStatus.DataError(1331, "请勿使用相同问题！");
             }
 
             var matter = new Matters
@@ -54,6 +60,43 @@ namespace Article.CMS.Api.Controllers
             _mattersRepository.Insert(matter);
             return DataStatus.DataSuccess(1000, matter, "问题添加成功！");
         }
+
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="PasswordInfo">传入前端数据实体</param>
+        /// <returns>是否成功</returns>
+        [HttpPut]
+        [Route("Changematter/{id}")]
+        public dynamic Changematter(int id, MatterParams newMatter)
+        {
+            var matter = _mattersRepository.GetId(id);
+
+            if (matter == null)
+            {
+                return DataStatus.DataError(1114, "数据不存在，无法执行修改操作！");
+            }
+
+            var mName = newMatter.MName.Trim();
+            if (string.IsNullOrEmpty(mName))
+            {
+                return DataStatus.DataError(1111, "请检查必填项目是否填写！");
+            }
+
+            var matters = _mattersRepository.Table.ToList();
+            var dbmattet = matters.Where(x => x.MName.Equals(mName)).SingleOrDefault();
+            if (dbmattet != null)
+            {
+                return DataStatus.DataError(1331, "请勿使用相同问题！");
+            }
+
+            matter.MName = mName;
+            _mattersRepository.Update(matter);
+
+            return DataStatus.DataSuccess(1000, matter, "修改成功");
+        }
+
 
     }
 }
