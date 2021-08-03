4438ed0a17f5ed946c24555e194776f2111adb11
SFY <1473209499@qq.com>
2021-08-03 15:32:30 +0800

    完成权限查询，添加接口，测试可用

--- Article.CMS.Api/Controllers/BackPowersController.cs
+++ Article.CMS.Api/Controllers/BackPowersController.cs
@@ -31,109 +31,120 @@ namespace Article.CMS.Api.Controllers
             _PowersRepository = PowersRepository;
         }
 
+        [HttpGet]
+        /// <summary>
+        /// 获取所有权限请求
+        /// </summary>
+        /// <returns></returns>
+        public dynamic Get()
+        {
+            var powers = _PowersRepository.Table.ToList();
+            return DataStatus.DataSuccess(1000, powers, "获取密保问题成功！");
+        }
+
         /// <summary>
         /// 添加权限
         /// </summary>
         /// <returns></returns>
-        [Route("AddMatter")]
+        [Route("AddPower")]
         [HttpPost]
-        public dynamic AddMatter(MatterParams newMatter)
+        public dynamic AddPower(PowerParams newPower)
         {
-            var mName = newMatter.MName.Trim();
-            var remarks = newMatter.Remarks == null ? null : newMatter.Remarks.Trim();
-            if (string.IsNullOrEmpty(mName))
+            var pName = newPower.PName.Trim();
+            var remarks = newPower.Remarks == null ? null : newPower.Remarks.Trim();
+            if (string.IsNullOrEmpty(pName))
             {
                 return DataStatus.DataError(1111, "请检查必填项目是否填写！");
             }
 
-            var matters = _mattersRepository.Table.ToList();
-            var dbmattet = matters.Where(x => x.MName.Equals(mName)).SingleOrDefault();
+            var matters = _PowersRepository.Table.ToList();
+            var dbmattet = matters.Where(x => x.PName.Equals(pName)).SingleOrDefault();
             if (dbmattet != null)
             {
-                return DataStatus.DataError(1331, "请勿使用相同问题！");
+                return DataStatus.DataError(1441, "已存在该权限！");
             }
 
-            var matter = new Matters
+            var Power = new Powers
             {
-                MName = mName,
+                PName = pName,
                 Remarks = remarks
 
             };
 
-            _mattersRepository.Insert(matter);
-            return DataStatus.DataSuccess(1000, matter, "问题添加成功！");
+            _PowersRepository.Insert(Power);
+            return DataStatus.DataSuccess(1000, Power, "权限添加成功！");
         }
 
-        /// <summary>
-        /// 修改问题
-        /// </summary>
-        /// <param name="id">用户id</param>
-        /// <param name="upMatter">传入前端数据实体</param>
-        /// <returns>是否成功</returns>
-        [HttpPut]
-        [Route("Changematter/{id}")]
-        public dynamic Changematter(int id, MatterParams upMatter)
-        {
-            var matter = _mattersRepository.GetId(id);
-            var remarks = upMatter.Remarks == null ? "" : upMatter.Remarks.Trim();
+        // /// <summary>
+        // /// 修改问题
+        // /// </summary>
+        // /// <param name="id">用户id</param>
+        // /// <param name="upMatter">传入前端数据实体</param>
+        // /// <returns>是否成功</returns>
+        // [HttpPut]
+        // [Route("Changematter/{id}")]
+        // public dynamic Changematter(int id, MatterParams upMatter)
+        // {
+        //     var matter = _mattersRepository.GetId(id);
+        //     var remarks = upMatter.Remarks == null ? "" : upMatter.Remarks.Trim();
 
 
-            if (matter == null)
-            {
-                return DataStatus.DataError(1114, "数据不存在，无法执行修改操作！");
-            }
+        //     if (matter == null)
+        //     {
+        //         return DataStatus.DataError(1114, "数据不存在，无法执行修改操作！");
+        //     }
 
-            var mName = upMatter.MName.Trim();
-            if (string.IsNullOrEmpty(mName))
-            {
-                return DataStatus.DataError(1111, "请检查必填项目是否填写！");
-            }
+        //     var mName = upMatter.MName.Trim();
+        //     if (string.IsNullOrEmpty(mName))
+        //     {
+        //         return DataStatus.DataError(1111, "请检查必填项目是否填写！");
+        //     }
 
-            var matters = _mattersRepository.Table.ToList();
-            var dbmattet = matters.Where(x => x.MName.Equals(mName) && x.Id!=id).SingleOrDefault();
-            if (dbmattet != null)
-            {
-                return DataStatus.DataError(1331, "请勿使用相同问题！");
-            }
+        //     var matters = _mattersRepository.Table.ToList();
+        //     var dbmattet = matters.Where(x => x.MName.Equals(mName) && x.Id!=id).SingleOrDefault();
+        //     if (dbmattet != null)
+        //     {
+        //         return DataStatus.DataError(1331, "请勿使用相同问题！");
+        //     }
 
-            matter.MName = mName;
-            matter.Remarks=remarks;
-            _mattersRepository.Update(matter);
+        //     matter.MName = mName;
+        //     matter.Remarks=remarks;
+        //     _mattersRepository.Update(matter);
 
-            return DataStatus.DataSuccess(1000, matter, "修改成功");
-        }
+        //     return DataStatus.DataSuccess(1000, matter, "修改成功");
+        // }
 
-        /// <summary>
-        /// 删除问题
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpDelete]
-        [Route("deleteMatter/{id}")]
-        public dynamic deleteMatter(int id)
-        {
-            var dbusersmatter = _Context.Users.Where(x => x.MatterId == id).Count();
-            if (dbusersmatter != 0)
-            {
-                return DataStatus.DataError(1332, "删除失败，问题使用中！");
-            }
+        // /// <summary>
+        // /// 删除问题
+        // /// </summary>
+        // /// <param name="id"></param>
+        // /// <returns></returns>
+        // [HttpDelete]
+        // [Route("deleteMatter/{id}")]
+        // public dynamic deleteMatter(int id)
+        // {
+        //     var dbusersmatter = _Context.Users.Where(x => x.MatterId == id).Count();
+        //     if (dbusersmatter != 0)
+        //     {
+        //         return DataStatus.DataError(1332, "删除失败，问题使用中！");
+        //     }
 
-            _mattersRepository.Delete(id);
-            return DataStatus.DataSuccess(1000, new { id = id }, "删除成功！");
-        }
+        //     _mattersRepository.Delete(id);
+        //     return DataStatus.DataSuccess(1000, new { id = id }, "删除成功！");
+        // }
 
-        /// <summary>
-        /// 模糊查询问题
-        /// </summary>
-        /// <param name="mName"></param>
-        /// <returns></returns>
-        [HttpGet]
-        [Route("likeMname/{mName}")]
-        public dynamic likeMname(string mName)
-        {
-            var dbLikeMName = _mattersRepository.Table.Where(x => x.MName.Contains(mName)).ToList();
-            return DataStatus.DataSuccess(1000, dbLikeMName, "查询成功！");
+        // /// <summary>
+        // /// 模糊查询问题
+        // /// </summary>
+        // /// <param name="mName"></param>
+        // /// <returns></returns>
+        // [HttpGet]
+        // [Route("likeMname/{mName}")]
+        // public dynamic likeMname(string mName)
+        // {
+        //     var dbLikeMName = _mattersRepository.Table.Where(x => x.MName.Contains(mName)).ToList();
+        //     return DataStatus.DataSuccess(1000, dbLikeMName, "查询成功！");
 
-        }
+        // }
     }
 }
---
+++ Article.CMS.Api/Params/PowerParams.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Article.CMS.Api.Entity
+{
+    /// <summary>
+    /// 前端权限实体
+    /// </summary>
+    public class PowerParams
+    {
+        //权限名称
+        public string PName { get; set; }
+        //备注
+        public string Remarks { get; set; }
+
+    }
+}
