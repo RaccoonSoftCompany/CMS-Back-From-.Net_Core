135d97438a6125a33aadf89f274c5218a6cfbe3e
SFY <1473209499@qq.com>
2021-08-04 15:00:34 +0800

    文章评论获取

---
+++ Article.CMS.Api/Params/Talkuser.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Article.CMS.Api.Entity
+{
+    /// <summary>
+    /// 前端评论用户实体
+    /// </summary>
+    public class Talkuser
+    {
+        //评论id
+        public int TalkId { get; set; }
+        //用户昵称
+        public string UNickName { get; set; }
+        //评论内容
+        public string TalkText { get; set; }
+
+    }
+}
